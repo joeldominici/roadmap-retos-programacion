3bd7afc3387c3ed4ad75c655ac56dee63ceadfce
joeldominici <joeldominicif@gmail.com>
2024-11-17 12:41:02 -0400

    Create joeldominici.py

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/joeldominici.py
@@ -0,0 +1,15 @@
+# https://www.python.org/ 
+""" Usando tres comillas dobles o tres comillas simples podemos 
+dejar plasmados comentarios de mas de una linea dentro de nuestro codigo
+"""
+#Crea una variable y una constante si el lenguaje lo permite
+Variable1 = "Hola Mundo"
+Constante1 = "Joel Dominici"
+"""Crea variables representando todos los tipos de datos primitivos 
+del lenguaje (cadenas de texto, enteros, booleanos...)
+"""
+String1 = "Hola Mundo"
+Int1 = 2024
+Boolean = True
+Float1 = 3.2
+Print(¡Hola, Python!)
