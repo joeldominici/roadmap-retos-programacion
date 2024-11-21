4a049d780801db018be8099179a2add788adfdcd
joeldominici <joeldominicif@gmail.com>
2024-11-20 21:50:27 -0400

    Create Joeldominici.py

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/Joeldominici.py
@@ -0,0 +1,23 @@
+#Operadores Aritmeticos 
+My_Num1 = 10
+My_Num2 = 5
+My_Suma = My_Num1 + My_Num2 #Operador de Suma
+My_Resta = My_Num1 - My_Num2 #Operador de Resta
+My_Mult = My_Num1 * My_Num2 #Operador de Multiplicacion
+My_Div = My_Num1 / My_Num2 #Operador de Division
+My_Modulo = My_Num1 % My_Num2 #Operador de Modulo
+My_Potencia = My_Num1 ** My_Num2
+My_IntDivision = My_Num1 // My_Num2
+
+Print(f"El resultado de la Suma es: {My_Suma}")
+Print(f"El resultado de la Resta es: {My_Resta}")
+Print(f"El resultado de la Multiplicacion es: {My_Mult}")
+Print(f"El resultado de la Division es: {My_Div}")
+Print(f"El resultado del cociente es: {My_Modulo}")
+Print(f"El resultado de la Potencia es: {My_Potencia}")
+Print(f"El resultado de la Division redondeada es: {My_IntDivision}")
+
+#Operadores Relacionales
+
+Prin(f"Operador Mayor que = {My_Num1 > My_Num2}")
+
